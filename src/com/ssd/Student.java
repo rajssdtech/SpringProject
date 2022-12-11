ef817863c208761eca5bb91124ba7dd3b79788b8
rajssdtech <rajkumarkappala80@gmail.com>
2022-12-11 11:59:25 +0530

    project template

--- src/com/ssd/Student.java
+++ src/com/ssd/Student.java
@@ -5,15 +5,23 @@ public class Student {
 	private Long id;
 	private String name;
 
+	
+	
 	public Long getId() {
 		return id;
 	}
+	
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	
 	public String getName() {
 		return name;
 	}
+	
+	
 	public void setName(String name) {
 		this.name = name;
 	}
